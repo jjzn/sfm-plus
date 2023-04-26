23e88b6cf78aaaf3c2dc37f8819c82f065aba2a9
jjzn <jjzn@protonmail.com>
2023-04-26 22:46:51 +0200

    Add caching to server to lessen load

--- server.py
+++ server.py
@@ -1,11 +1,14 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from urllib.parse import urlparse
+from datetime import datetime
 import json
 import info
 
 PORT = 8420
 
 class TrainServer(BaseHTTPRequestHandler):
+    cache = {}
+
     def do_GET(self):
         code_str = self.path.split('/')[-1]
         code = int(code_str)
@@ -13,10 +16,18 @@ class TrainServer(BaseHTTPRequestHandler):
         # ensure code is valid
         assert list(info.codes.values()).count(code) == 1
 
-        res = json.dumps(info.retrieve_info(code))
+        now = datetime.now()
+        t = now.hour * 60 + now.minute
+
+        if code in self.cache and self.cache[code][0] >= t:
+            res = self.cache[code][1]
+        else:
+            res = json.dumps(info.retrieve_info(code))
+            self.cache[code] = (t, res)
 
         self.send_response(200)
         self.send_header('Content-Type', 'text/json')
+        self.send_header('Access-Control-Allow-Origin', '*')
         self.end_headers()
 
         self.wfile.write(bytes(res + '\n', 'utf-8'))
