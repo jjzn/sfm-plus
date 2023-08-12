bd3f2a01b4859a4a067c1b342271ababd7424e43
jjzn <50965831+jjzn@user.noreply.github.com>
2023-08-12 14:16:21 +0200

    Reimplement the train info module in Rust

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
 #[macro_use] extern crate rocket;
 
+mod train_info;
+
+const SFM_IMAGE_BASE_URL: &str = "https://info.trensfm.com/sapi/ivi_imagen";
+
 #[get("/<code>")]
 fn info(code: u8) -> String {
-    format!("Not implemented. Code {}", code)
+    let url = format!("{}?ubicacion={}", SFM_IMAGE_BASE_URL, code);
+    let trains = train_info::retrieve(&url);
+
+    format!("Found {} trains at station {}", trains.len(), code)
 }
 
 #[launch]
---
+++ src/train_info.rs
@@ -0,0 +1,55 @@
+use rusty_tesseract::Image;
+use std::io::Read;
+
+const MAX_IMAGE_BYTES: usize = 10_000_000; // 10 MB
+
+pub struct TrainTime {
+    hour: u8,
+    minute: u8
+}
+
+pub struct Train {
+    headsign: String,
+    time: TrainTime,
+    track: u8
+}
+
+fn split_region(path: &str, idx: u8) -> (Image, Image) {
+    let response = ureq::get(path).call().unwrap();
+
+    let len: usize = response
+        .header("Content-Length")
+        .map(|s| s.parse().unwrap())
+        .unwrap_or(MAX_IMAGE_BYTES);
+
+    let mut bytes = Vec::with_capacity(len);
+    response
+        .into_reader()
+        .take(MAX_IMAGE_BYTES as u64)
+        .read_to_end(&mut bytes);
+
+    let img = image::load_from_memory(&bytes).unwrap();
+    let tess_img = Image::from_dynamic_image(&img).unwrap();
+
+    (tess_img, tess_img)
+}
+
+pub fn retrieve(path: &str) -> Vec<Train> {
+    for i in 0..7 {
+        let (name_img, rest_img) = split_region(path, i);
+        let tess_args = rusty_tesseract::Args {
+            config_variables: std::collections::HashMap::new(),
+            lang: "eng".into(),
+            dpi: Some(300),
+            psm: Some(11),
+            oem: None
+        };
+
+        let name = rusty_tesseract::image_to_string(&name_img, &tess_args)
+            .unwrap().trim();
+        let rest = rusty_tesseract::image_to_string(&rest_img, &tess_args)
+            .unwrap().trim();
+    }
+
+    vec![]
+}
