3f98a256eccce95b2a9cd8e62927997783cb08f1
jjzn <50965831+jjzn@user.noreply.github.com>
2023-08-12 22:13:36 +0200

    Implement image splitting + transformation
    Currently, only the 'name' image is transformed. A future commit will
    address this.

--- src/train_info.rs
+++ src/train_info.rs
@@ -1,6 +1,6 @@
 use rusty_tesseract::Image;
-use image::GenericImageView;
 use opencv::imgproc::*;
+use opencv::core::{Mat, Point, Size, Vector, BORDER_CONSTANT};
 use std::io::Read;
 
 const MAX_IMAGE_BYTES: usize = 10_000_000; // 10 MB
@@ -18,6 +18,20 @@ pub struct Train {
     track: u8
 }
 
+fn vector_to_image(vector: Vector<u8>) -> rusty_tesseract::Image {
+    let img = image::load_from_memory(&vector.to_vec()).unwrap();
+
+    rusty_tesseract::Image::from_dynamic_image(&img).unwrap()
+}
+
+fn structuring_rect(width: i32, height: i32) -> Mat {
+    get_structuring_element(
+            MORPH_RECT,
+            Size { width, height },
+            Point::new(-1, -1))
+        .unwrap()
+}
+
 fn split_region(path: &str, idx: u32) -> (Image, Image) {
     let response = ureq::get(path).call().unwrap();
 
@@ -27,21 +41,47 @@ fn split_region(path: &str, idx: u32) -> (Image, Image) {
         .unwrap_or(MAX_IMAGE_BYTES);
 
     let mut bytes = Vec::with_capacity(len);
-    response
+    let _ = response
         .into_reader()
         .take(MAX_IMAGE_BYTES as u64)
         .read_to_end(&mut bytes);
 
     let mut img = image::load_from_memory(&bytes).unwrap();
 
-    let name_img = image::imageops::crop(
+    let mut name_img: Vector<_> = image::imageops::crop(
             &mut img,
             0, IMAGE_ELEMENT_OFFSET + idx * IMAGE_ELEMENT_HEIGHT,
             342, IMAGE_ELEMENT_HEIGHT)
         .to_image()
-        .as_raw();
+        .into_raw()
+        .into();
 
-    name_img
+    let mut rest_img: Vector<_> = image::imageops::crop(
+            &mut img,
+            342, IMAGE_ELEMENT_OFFSET + idx * IMAGE_ELEMENT_HEIGHT,
+            427, IMAGE_ELEMENT_HEIGHT)
+        .to_image()
+        .into_raw()
+        .into();
+
+    let _ = cvt_color(&name_img.clone(), &mut name_img, COLOR_RGB2GRAY, 0);
+    let _ = cvt_color(&rest_img.clone(), &mut rest_img, COLOR_RGB2GRAY, 0);
+
+    let _ = dilate(
+        &name_img.clone(), &mut name_img, &structuring_rect(3, 3),
+        Point::new(-1, -1), 1,
+        BORDER_CONSTANT, morphology_default_border_value().unwrap());
+
+    let _ = median_blur(&name_img.clone(), &mut name_img, 7);
+
+    let _ = erode(
+        &name_img.clone(), &mut name_img, &structuring_rect(5, 5),
+        Point::new(-1, -1), 1,
+        BORDER_CONSTANT, morphology_default_border_value().unwrap());
+
+    let _ = threshold(&name_img.clone(), &mut name_img, 0., 255., THRESH_OTSU);
+
+    (vector_to_image(name_img), vector_to_image(rest_img))
 }
 
 pub fn retrieve(path: &str) -> Vec<Train> {
