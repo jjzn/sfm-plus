c96a45854366d1f48a4aebf1c5f779fbbf6fe664
jjzn <50965831+jjzn@user.noreply.github.com>
2023-09-05 23:45:44 +0200

    Add middleware to provide CORS headers

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,27 @@
 #[macro_use] extern crate rocket;
 
 use rocket::serde::json::Json;
+use rocket::fairing::{Fairing, Info, Kind};
 
 mod train_info;
 
 const SFM_IMAGE_BASE_URL: &str = "https://info.trensfm.com/sapi/ivi_imagen";
+
+struct Cors();
+
+#[rocket::async_trait]
+impl Fairing for Cors {
+    fn info(&self) -> Info {
+        Info {
+            name: "CORS headers",
+            kind: Kind::Response
+        }
+    }
+
+    async fn on_response<'r>(&self, _: &'r rocket::Request<'_>, res: &mut rocket::Response<'r>) {
+        res.adjoin_raw_header("Access-Control-Allow-Origin", "*");
+    }
+}
 
 #[get("/<code>")]
 fn info(code: u8) -> Json<Vec<train_info::Train>> {
@@ -15,5 +32,7 @@ fn info(code: u8) -> Json<Vec<train_info::Train>> {
 
 #[launch]
 async fn server() -> _ {
-    rocket::build().mount("/", routes![info])
+    rocket::build()
+        .attach(Cors {})
+        .mount("/", routes![info])
 }
