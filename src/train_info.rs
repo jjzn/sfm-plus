5db351e2e9a10ec908886a778c669bea8643c147
jjzn <50965831+jjzn@user.noreply.github.com>
2023-09-05 20:43:58 +0200

    Parse strings as `TrainTime`s

--- src/train_info.rs
+++ src/train_info.rs
@@ -26,6 +26,39 @@ pub struct TrainTime {
     minute: u8
 }
 
+#[derive(Debug)]
+pub enum TimeError {
+    MissingSeparator,
+    InvalidComponent
+}
+
+impl std::error::Error for TimeError {}
+
+impl std::fmt::Display for TimeError {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "{}", match self {
+            Self::MissingSeparator => "Missing component separator (':')",
+            Self::InvalidComponent => "Invalid component (cannot be parsed as a number)"
+        })
+    }
+}
+
+impl TryFrom<String> for TrainTime {
+    type Error = TimeError;
+
+    fn try_from(val: String) -> Result<Self, Self::Error> {
+        let (h, m) = val
+            .split_once(':')
+            .ok_or(Self::Error::MissingSeparator)?;
+
+        let hour: u8 = h.parse().map_err(|_| Self::Error::InvalidComponent)?;
+        let minute: u8 = m.parse().map_err(|_| Self::Error::InvalidComponent)?;
+
+        Ok(Self { hour, minute })
+    }
+}
+
+#[derive(Debug)]
 pub struct Train {
     headsign: String,
     time: TrainTime,
