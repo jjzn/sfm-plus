1a7581867397d9056ba26a591b3ecb5a3d5fcd4c
jjzn <50965831+jjzn@users.noreply.github.com>
2023-11-16 19:15:18 +0100

    Include lat/lon for SFM stations

--- static/stops/sfm.js
+++ static/stops/sfm.js
@@ -1,147 +1,147 @@
 export default {
   "sfm/1": {
-    "lat": 0,
-    "lon": 0,
+    "lat": 39.5764574,
+    "lon": 2.6543586,
     "name": "Intermodal"
   },
   "sfm/2": {
-    "lat": 0,
-    "lon": 0,
+    "lat": 39.5800542,
+    "lon": 2.6599692,
     "name": "Jacint Verdaguer"
   },
   "sfm/3": {
-    "lat": 0,
-    "lon": 0,
+    "lat": 39.5841700,
+    "lon": 2.6657937,
     "name": "Son Costa"
   },
   "sfm/54": {
-    "lat": 0,
-    "lon": 0,
+    "lat": 39.5954593,
+    "lon": 2.6715156,
     "name": "Son Fuster Vell"
   },
   "sfm/55": {
-    "lat": 0,
-    "lon": 0,
+    "lat": 39.6023496,
+    "lon": 2.6722551,
     "name": "Son Castelló"
   },
   "sfm/56": {
-    "lat": 0,
-    "lon": 0,
+    "lat": 39.6063852,
+    "lon": 2.6687509,
     "name": "Gran Via Asima"
   },
   "sfm/57": {
-    "lat": 0,
-    "lon": 0,
+    "lat": 39.6102917,
+    "lon": 2.6653561,
     "name": "Camí dels Reis"
   },
   "sfm/58": {
-    "lat": 0,
-    "lon": 0,
+    "lat": 39.6206563,
+    "lon": 2.6612228,
     "name": "Son Sardina"
   },
   "sfm/51": {
-    "lat": 0,
-    "lon": 0,
+    "lat": 39.6346089,
+    "lon": 2.6480014,
     "name": "UIB"
   },
   "sfm/4": {
-    "lat": 0,
-    "lon": 0,
+    "lat": 39.5902008,
+    "lon": 2.6746056,
     "name": "Son Fuster"
   },
   "sfm/30": {
-    "lat": 0,
-    "lon": 0,
+    "lat": 39.5934854,
+    "lon": 2.6793384,
     "name": "Son Cladera"
   },
   "sfm/6": {
-    "lat": 0,
-    "lon": 0,
+    "lat": 39.5967658,
+    "lon": 2.6863945,
     "name": "Verge de Lluc"
   },
   "sfm/7": {
-    "lat": 0,
-    "lon": 0,
+    "lat": 39.5981200,
+    "lon": 2.6917323,
     "name": "Pont d'Inca"
   },
   "sfm/8": {
-    "lat": 0,
-    "lon": 0,
+    "lat": 39.6044921,
+    "lon": 2.7015526,
     "name": "Pont d'Inca Nou"
   },
   "sfm/9": {
-    "lat": 0,
-    "lon": 0,
+    "lat": 39.6155242,
+    "lon": 2.7167820,
     "name": "Polígon de Marratxí"
   },
   "sfm/10": {
-    "lat": 0,
-    "lon": 0,
+    "lat": 39.6253908,
+    "lon": 2.7254749,
     "name": "Marratxí"
   },
   "sfm/11": {
-    "lat": 0,
-    "lon": 0,
+    "lat": 39.6320407,
+    "lon": 2.7364873,
     "name": "Es Caülls"
   },
   "sfm/12": {
-    "lat": 0,
-    "lon": 0,
+    "lat": 39.6531744,
+    "lon": 2.7740875,
     "name": "Santa Maria"
   },
   "sfm/13": {
-    "lat": 0,
-    "lon": 0,
+    "lat": 39.6776664,
+    "lon": 2.8086522,
     "name": "Consell - Alaró"
   },
   "sfm/14": {
-    "lat": 0,
-    "lon": 0,
+    "lat": 39.6924449,
+    "lon": 2.8411893,
     "name": "Binissalem"
   },
   "sfm/15": {
-    "lat": 0,
-    "lon": 0,
+    "lat": 39.7163416,
+    "lon": 2.8691844,
     "name": "Lloseta"
   },
   "sfm/16": {
-    "lat": 0,
-    "lon": 0,
+    "lat": 39.7181030,
+    "lon": 2.9058376,
     "name": "Inca"
   },
   "sfm/17": {
-    "lat": 0,
-    "lon": 0,
+    "lat": 39.6906676,
+    "lon": 2.9512554,
     "name": "Enllaç"
   },
   "sfm/18": {
-    "lat": 0,
-    "lon": 0,
+    "lat": 39.7113769,
+    "lon": 2.9940250,
     "name": "Llubí"
   },
   "sfm/19": {
-    "lat": 0,
-    "lon": 0,
+    "lat": 39.7340175,
+    "lon": 3.0319461,
     "name": "Muro"
   },
   "sfm/20": {
-    "lat": 0,
-    "lon": 0,
+    "lat": 39.7644614,
+    "lon": 3.0292548,
     "name": "Sa Pobla"
   },
   "sfm/21": {
-    "lat": 0,
-    "lon": 0,
+    "lat": 39.6437029,
+    "lon": 3.0143689,
     "name": "Sineu"
   },
   "sfm/23": {
-    "lat": 0,
-    "lon": 0,
+    "lat": 39.6181708,
+    "lon": 3.1109092,
     "name": "Petra"
   },
   "sfm/24": {
-    "lat": 0,
-    "lon": 0,
+    "lat": 39.5705578,
+    "lon": 3.2028380,
     "name": "Manacor"
   }
 };
